7dad2c4c0c545155ce95138017964197b65bcb94
JOE5KI <55300869+JOE5KI@users.noreply.github.com>
2020-07-26 13:12:53 -0700

    JewelryRob

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {} -- bitch
+
+Config.HulkSmash = 4000  -- 4 sec to smash box 
+
+Config.ReggieTimer = 30 -- 30 min to reset boxes
+
+Config.PoliceAmount = 0 -- needed to start 
+
+Config.AlartPercentPerBox = 25  -- 25% for alarm to go off per box 
+
+Config.blip = 77
+Config.blipColor = 1 
+Config.ShowStore = true 
